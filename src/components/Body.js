513502b861fe94ed6c4f808777078c5a6ac58c8f
Vaibhav Mishra <vaibhav.mishra245@gmail.com>
2024-09-17 18:54:51 +0530

    Router and basic component development

---
+++ src/components/Body.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Login from './Login';
+import Browser from './Browser';
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<Login/>
+    },
+    {
+        path:"/browser",
+        element:<Browser/>
+    }
+  ])
+  return (
+    <div>
+        <RouterProvider router={appRouter}/>
+    </div>
+  )
+}
+
+export default Body
