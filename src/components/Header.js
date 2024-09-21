3efeeab31daeac7646badf797b4264704f3ff87d
Vaibhav Mishra <vaibhav.mishra245@gmail.com>
2024-09-21 17:36:23 +0530

    SignUp/SIgnIn form creation

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute w-44 px-2 mx-2'>  w-44 px-2 mx-2
-        <img src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+    <div className='absolute px-2 mx-2 bg-gradient-to-b from-black z-20'>
+        <img className='w-44 '
+             src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
              alt='netflix-logo' />
     </div>
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+
+    const[isSignInForm , setIsSignInForm] = useState(true);
+
+    const toggleForm  = ()=>{
+        setIsSignInForm(!isSignInForm);
+    }
+
     return (
         <div>
             <Header />
-            <div>
+            <div className='absolute'>
                 <img src='https://assets.nflxext.com/ffe/siteui/vlv3/85ff76db-39e5-423a-afbc-97d3e74db71b/null/IN-en-20240909-TRIFECTA-perspective_b22117e0-4610-4d57-a695-20f77d241a4a_small.jpg'
-                      alt='netflix-background-img' />
+                    alt='netflix-background-img' />
 
-        </div>
+            </div>
+            <form className='w-3/12 absolute bg-black p-12 py-4 my-36 mx-auto right-0 left-0 text-white opacity-80 rounded-lg'>
+              <h1 className='font-bold text-3xl py-4'> {isSignInForm ?  "Sign In" : "Sign Up" }</h1>
+              {
+                !isSignInForm && 
+                <input 
+                className='w-full p-4 my-4 bg-gray-600' 
+                type="text" 
+                placeholder='Full Name'/>
+              }
+                <input 
+                className='w-full p-4 my-4 bg-gray-600' 
+                type="text" 
+                placeholder='Email Id'/>
+                <input className='w-full p-4 my-4 bg-gray-600' 
+                type='password' 
+                placeholder='Password' /> 
+
+                <button className='bg-red-600 w-full my-6 p-4 rounded-lg'>
+                    {isSignInForm ?  "Sign In" : "Sign Up"}
+                </button>
+                <p> {isSignInForm ?  "New to Netflix ? " : "Already registered ? "} <span className='text-yellow-500' onClick={toggleForm}>{isSignInForm ?  "Sing Up Now" : "Sign In"}</span></p>
+            </form>
         </div>
     )
 }
